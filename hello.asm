e65daca198206ef6254c803e629670e2deca5dcd
Swapnil Mishra <63197684+swap72@users.noreply.github.com>
2026-04-28 16:36:08 +0530

    Going

--- hello.asm
+++ hello.asm
@@ -1,18 +1,7 @@
-section .data
-    hello db 'Hello, World!',0
+package main
 
-section .text
-    global _start
+import "fmt"
 
-_start:
-    ; Write "Hello, World!" to stdout
-    mov eax, 4         ; syscall number for sys_write
-    mov ebx, 1         ; file descriptor (stdout)
-    mov ecx, hello    ; pointer to the message
-    mov edx, 13        ; message length
-    int 0x80           ; interrupt to invoke syscall
-
-    ; Exit the program
-    mov eax, 1         ; syscall number for sys_exit
-    xor ebx, ebx       ; exit code 0
-    int 0x80           ; interrupt to invoke syscall
+func main(){
+    fmt.Println("Hello Universe!);
+}
